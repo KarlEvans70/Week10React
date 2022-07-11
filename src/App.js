a05cbb9915e167f04e3da39403675b96e616de95
Karl Evans <karlevans70@hotmail.com>
2022-07-11 15:03:29 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Box from "./box";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState("Steve");
+  const [photos, setPhotos] = useState([]);
+
+  const arr = [{ name: "Steve" }, { name: "Gary" }, { name: "Tony" }, { name: "Kevin" }]; 
+
+  const fetchImages = async () => {
+    const response = await fetch("https://picsum.photos/v2/list");
+    const data = await response.json();
+    setPhotos(data);
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, [user]); //everytime user's value changes, useEffect will run again
+
+
+  // for(let i = 0; i < arr.length; i++) {
+  //   console.log(arr[i]);
+  // };
+
+  // instead of doing the above you can do the below
+
+  // arr.map((item, index) => {
+  //   console.log(item.name)
+  // })
+
+  // const useState = (initialVal) => {
+  //   let state = initialVal;
+
+  //   const setState = (newVal) => {
+  //     state = newVal
+  //   }
+
+  //   return [state, setState];
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {photos.map((item, index) => {
+        return(
+          <div>
+            <img src={item.download_url} />
+          </div>
+        )
+      })}
+      {/* <Box name={user} />
+      {arr.map((item, i) => {
+        return (
+          <div>
+            <Box name={item.name} />
+        ) </div>
+        );
+      })}
+      <input onChange={(event) => setUser(event.target.value)} />
+      {user && <Box name="Tony" />}
+      {user ? <Box name="Jeff" /> : <Box name="Not Jeff" />} */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/box.js
@@ -0,0 +1,9 @@
+const Box = ({name}) => {
+    return (
+      <div className="Box">
+        <h1>{name}</h1>
+      </div>
+    );
+};
+
+export default Box;
